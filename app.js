f678cc51921b936f91f36bac5919e3cfa1838478
Meelad Raza <meeladraza929@gmail.com>
2026-01-08 23:31:46 +0500

    Country Flag Finder using REST API

---
+++ app.js
@@ -0,0 +1,29 @@
+
+let input = document.getElementById('input');
+let flagImg = document.getElementById('flagImg');
+let alertPara = document.getElementById('alertPara');
+let btn = document.getElementById('btn')
+
+
+btn.addEventListener('click', () => {
+    if (!input.value) {
+        alertPara.style.display = 'inline-block'
+        alertPara.textContent = 'Enter country name (e.g. Pakistan)'
+        return;
+    };
+
+    let api = fetch(`https://restcountries.com/v3.1/name/${input.value}`)
+        .then(res => res.json())
+        .then((result) => {
+            alertPara.style.display = 'none'
+            let [{ flags: { png } }] = result
+            flagImg.src = png
+        }).catch(() => {
+            alertPara.style.display = 'inline-block'
+            alertPara.textContent = 'You Enter Invalid Country'
+            flagImg.src = ''
+        })
+})
+
+
+
